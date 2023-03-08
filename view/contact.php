9b690773f8ca007c540add2145cd49a8e86e7d46
ErrazIG <frederic-vml@hotmail.com>
2023-03-08 18:46:19 +0100

    contact

--- view/contact.php
+++ view/contact.php
@@ -13,7 +13,32 @@
 
 <body>
   <?php include_once '../include/menu.php' ?>
+
   <h1>CONTACT</h1>
+
+<div class="form">
+	<div class="form-content">
+		<div class="form-child">
+			<label>Nom</label>
+			<input type="text" name="" placeholder="Nom">
+		</div>
+		<div class="form-child">
+			<label>Prénom</label>
+			<input type="text" name="" placeholder="Prénom">
+		</div>
+		<div class="form-child">
+			<label>E-mail</label>
+			<input type="mail" name="" placeholder="Nom">
+		</div>
+		<div class="form-child">
+			<label>Message</label>
+			<textarea name="" class="txtarea"></textarea>
+		</div>
+		<div class="form-child">
+			<input type="submit" name="submit" value="Envoyer">
+		</div>
+	</div>
+</div>
 
 
 
