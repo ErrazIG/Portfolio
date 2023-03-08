56b129673266539d97e838d669b7d766ad1e0cfe
ErrazIG <Frederic-vml@hotmail.com>
2023-03-08 13:27:40 +0100

    switch

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+if(isset($_GET['p'])){
+    
+    switch($_GET['p']){
+        case 'home':
+            include 'view/homepage.php';
+            break;
+        case 'work':
+            include 'view/work.php';
+            break;
+        case 'tuto':
+            include "view/tuto.php";
+                break;
+        case 'links':
+            include 'view/links.php';
+                break;
+        case 'cv':
+            include 'view/cv.php';
+                break;
+        case 'contact':
+            include 'view/contact.php';
+                break;
+        case 'admin':
+            include 'view/admin.php';
+                break;
+        default:
+            include_once 'view/homepage.php';
+    }
+  
+}else{
+    include_once "view/homepage.php";
+}
